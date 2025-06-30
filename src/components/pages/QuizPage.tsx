3689180c8228c6295911b3a2ec1a6848aa24ccbe
nori.hasimo.89@gmail.com <nori.hasimo.89@gmail.com>
2025-07-01 00:47:33 +0900

    クイズ画面とボタン作成

--- src/components/pages/QuizPage.tsx
+++ src/components/pages/QuizPage.tsx
@@ -1,13 +1,19 @@
 import quizData  from '../../data/quiz';
+import Button  from '../Button/Button';
 import Display from '../Display/Display';
 
 export default function QuizPage(){
     const quizIndex = 0;
     return (
-        <Display>
-            <div>
-                <p>{`Q1.${quizData[quizIndex].question}`}</p>
-            </div>
-        </Display>
+        <>
+            <Display>
+                {`Q1. ${quizData[quizIndex].question}`}
+            </Display>
+            <Button>{quizData[quizIndex].options[0]}</Button>
+            <Button>{quizData[quizIndex].options[1]}</Button>
+            <Button>{quizData[quizIndex].options[2]}</Button>
+            <Button>{quizData[quizIndex].options[3]}</Button>
+            <Button>{quizData[quizIndex].options[4]}</Button>
+        </>
     )
 }
