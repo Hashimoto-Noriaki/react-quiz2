c6cebf8767f7199e32b3807c837cb5d395031772
nori.hasimo.89@gmail.com <nori.hasimo.89@gmail.com>
2025-07-02 22:22:34 +0900

    データ作成

---
+++ src/data/quiz.ts
@@ -0,0 +1,35 @@
+export interface Quiz {
+    question: string;
+    options: string[];
+    answerIndex: number;
+}
+
+const quizData: Quiz[] = [
+    {
+        question:'ルフィが食べた悪魔の実の名前は？',
+        options:['ゴムゴムの実','メラメラの実','バラバラの実','ピカピカの実','ヒエヒエの実'],
+        answerIndex:0,
+    },
+    {
+        question:'ナルトの必殺技は？',
+        options:['千鳥','螺旋丸','火遁・豪火球の術','水遁・水龍弾の術','土遁・土砂崩し'],
+        answerIndex:1,
+    },
+    {
+        question:'ヒトカゲの最終進化は？',
+        options:['バシャーモ','ゴウカザル','リザードン','ファイアー','バグフーン'],
+        answerIndex:2,
+    },
+    {
+        question:'エヴァンゲリオンのヒロインは?',
+        options:['ハンコック','蘭','ナミ','アスカ','サクラ'],
+        answerIndex:3,
+    },
+    {
+        question:'スラムダンクの連載が終了した年は？',
+        options:['1995年','1997年','1998年','1996年','1999年'],
+        answerIndex:3,
+    }
+]
+
+export default quizData;
