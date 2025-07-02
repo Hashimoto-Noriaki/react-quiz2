b487c33657e387529b2c334feb5fa82335466cca
nori.hasimo.89@gmail.com <nori.hasimo.89@gmail.com>
2025-07-02 23:02:34 +0900

    結果画面

--- src/components/pages/ResultPage.tsx
+++ src/components/pages/ResultPage.tsx
@@ -1,5 +1,17 @@
+import { Link,useLocation } from 'react-router-dom'
+import { ROUTES } from '../../const'
+
 export default function ResultPage () {
+    const location = useLocation();
+    const maxQuizLen = location.state.maxQuizLen;
+    const correctNumLen = location.state.correctNumLen;
     return (
-        <div>結果発表</div>
+        <>
+            <h1>結果発表</h1>
+            <p>あなたの正解数は...</p>
+            <p>{`${maxQuizLen}問中、${correctNumLen}問正解です！`}</p>
+            <Link to={ROUTES.QUIZ}>もう一度クイズに挑戦</Link>
+            <Link to={ROUTES.HOME}>トップページへ戻る</Link>
+        </>
     )
 }
