3e6a0c0b87b8a6e89912ea1d5011f5fff85b53f2
nori.hasimo.89@gmail.com <nori.hasimo.89@gmail.com>
2025-07-02 19:39:18 +0900

    クイズ画面に遷移するリンクを配置

--- src/components/pages/TopPage.tsx
+++ src/components/pages/TopPage.tsx
@@ -1,10 +1,11 @@
 import { Link } from 'react-router-dom';
+import { ROUTES } from '../../const';
 
 export default function TopPage() {
     return (
         <>
             <h1>トップページ</h1>
-            <Link>クイズにに挑戦</Link>
+            <Link to={ROUTES.QUIZ}>クイズにに挑戦</Link>
         </>
 
     )
