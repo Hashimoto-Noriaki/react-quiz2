e3ce917843ca406aa5f33456b83aa455ade3ca74
nori.hasimo.89@gmail.com <nori.hasimo.89@gmail.com>
2025-07-02 23:18:53 +0900

    リファクタリング

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -3,7 +3,9 @@ import styles from './Button.module.css'
 export default function Button({children,onClick}) {
     return (
         <button className={styles.button} onClick={onClick}>
-            {children}
+            <span className={styles.buttonInner}>
+                {children}
+            </span>
         </button>
     )
 }
