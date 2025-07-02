deab6fb3e0733707fd9c0eb912860dcb91b3eee9
nori.hasimo.89@gmail.com <nori.hasimo.89@gmail.com>
2025-07-02 19:38:17 +0900

    トップページ

--- src/components/pages/TopPage.tsx
+++ src/components/pages/TopPage.tsx
@@ -1,5 +1,11 @@
+import { Link } from 'react-router-dom';
+
 export default function TopPage() {
     return (
-        <div>トップページ</div>
+        <>
+            <h1>トップページ</h1>
+            <Link>クイズにに挑戦</Link>
+        </>
+
     )
 }
