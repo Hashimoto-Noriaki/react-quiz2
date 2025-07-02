5d5afb02aa7ee3a9ef67e68e307d91595ea4e8aa
nori.hasimo.89@gmail.com <nori.hasimo.89@gmail.com>
2025-07-02 19:24:01 +0900

    クイズアプリ作成

--- src/components/pages/ResultPage.tsx
+++ src/components/pages/ResultPage.tsx
@@ -1,7 +1,16 @@
-
+import { Link,useLocation } from 'react-router-dom';
+import { ROUTES } from '../../const';
+import Result from '../Result/Result';
 
 export default function ResultPage() {
+    const location = useLocation();
+    const maxQuizLen = location.state.maxQuizLen
+    const correctNumLen = location.state.correctNumLen
     return (
-        <div>結果発表</div>
+        <>
+            <h1>結果発表</h1>
+            <Result maxQuizLen={maxQuizLen} correctNumLen={correctNumLen}/>
+            <Link to={ROUTES.QUIZ}>もう一度チャレンジ</Link>
+        </>
     )
 }
--- src/components/pages/TopPage.tsx
+++ src/components/pages/TopPage.tsx
@@ -1,8 +1,12 @@
-
+import { Link } from 'react-router-dom';
+import { ROUTES } from '../../const';
 
 export default function TopPage() {
     return (
-        <div>トップページ</div>
+        <>
+            <h1>QuizApp</h1>
+            <Link to={ROUTES.QUIZ}>クイズにチャレンジ</Link>
+        </>
     )
 }
 
