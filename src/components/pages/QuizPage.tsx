5f4646bebe77505d364a851357d3692e82e4a5f0
nori.hasimo.89@gmail.com <nori.hasimo.89@gmail.com>
2025-07-02 19:52:57 +0900

    DisplayとButtonを追加してクイズ画面

--- src/components/pages/QuizPage.tsx
+++ src/components/pages/QuizPage.tsx
@@ -1,5 +1,21 @@
+import quizData  from '../../data/quiz';
+import Display from '../../Display/Display';
+import Button from '../Button/Button';
+
 export default function QuizPage() {
+    const quizIndex = 0;
     return (
-        <div>クイズ</div>
+        <>
+            <Display>
+                <div>
+                    {`Q1.${quizData[quizIndex].question}`}
+                </div>
+            </Display>
+            <Button>{`${quizData[quizIndex].options[0]}`}</Button>
+            <Button>{`${quizData[quizIndex].options[1]}`}</Button>
+            <Button>{`${quizData[quizIndex].options[2]}`}</Button>
+            <Button>{`${quizData[quizIndex].options[3]}`}</Button>
+            <Button>{`${quizData[quizIndex].options[4]}`}</Button>
+        </>
     )
 }
